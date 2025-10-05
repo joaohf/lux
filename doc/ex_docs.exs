fa24ee90f4882a4aec17c04c4261e973f976a429
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2025-10-05 20:46:53 -0300

    Introduce ex_doc

---
+++ doc/ex_docs.exs
@@ -0,0 +1,20 @@
+[
+  ## The order of these items determine
+  ## how they are listed in the docs
+  extras: [
+    "intro.md",
+    "main_concepts.md",
+    "../tutorial/README.md",
+    "script_syntax.md",
+    "cmd_line_opts.md",
+    "config_params.md",
+    "logs.md",
+    "debug_cmds.md",
+    "examples.md",
+    "hardening.md",
+    "warnings.md",
+    "../INSTALL.md",
+    "../AUTHORS.md",
+    "references.md"
+  ]
+]
